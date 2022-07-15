a3ffcb76ae84347b1f31f5c7c49bc7f7ae73ed0d
mseob <kimaudtjq@kyonggi.ac.kr>
2022-07-15 16:46:17 +0900

    Get URL from Channel

--- main.py
+++ main.py
@@ -4,44 +4,37 @@ from bs4 import BeautifulSoup
 from datetime import datetime
 import time
 
-channel = "https://www.youtube.com/channel/UCHt_gfJhwWgFPE1HtG1Y-hA"
+channel = "https://www.youtube.com/channel/UCHt_gfJhwWgFPE1HtG1Y-hA/videos"
+youtube_url = "https://www.youtube.com"
 contentList = []
 
 driver = webdriver.Chrome()
-page = driver.page_source
-driver.get(channel+"/videos")
 driver.maximize_window()
-
-soup = BeautifulSoup(page, 'lxml')
+driver.get(channel)
 
 
 lsat_height = driver.execute_script(
     "return document.documentElement.scrollHeight")
 
+last_element = []
 while True:
     driver.execute_script(
-        "window.scrollTo(0, document.documentElement.scrollHeight);")
+        "window.scrollTo(0,document.documentElement.scrollHeight);")
 
     time.sleep(5)
+    page = driver.page_source
+    soup = BeautifulSoup(page, 'lxml')
 
-    new_height = driver.execute_script(
-        "return document.documentElement.scrollHeight")
-    if new_height == lsat_height:
-        break
-    lsat_height = new_height
+    video_data = soup.find_all(
+        'ytd-grid-video-renderer', {'class': 'style-scope ytd-grid-renderer'})
 
-    for i in range(len(lsat_height)):
+    video_url_list = []
+    for i in range(len(video_data)):
 
-        title = soup.find_all(
-            'a', 'yt-simple-endpoint style-scope ydt-grid-video-renderer')[i]['title']
-        url = soup.find_all(
-            'a', 'yt-simple-endpoint style-scope ydt-grid-video-renderer')[i]['href']
+        url = youtube_url + \
+            video_data[i].find('a', {'id': 'thumbnail'})['href']
+        video_url_list.append(url)
+    print(video_url_list)
 
-        contentList.append({
-            "제목": title,
-            "url": url,
-            "업로드 날짜": 0,
-            "조회수": 0,
-            "댓글": 0
-        })
-        print(i, title, url)
+    dataframe = pd.DataFrame(
+        {'name': [], 'view_count': [], 'youtube_url': [], 'date': [], 'desc': []})
