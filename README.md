103ca94e62a7e18b846af9a2f1b725b25905a79b
mseob <kimaudtjq@kyonggi.ac.kr>
2022-07-17 16:54:19 +0900

    Done :)

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-유튜브 크롤링
+## Youtube 크롤링 RPA
+
+#
+
+Python(정보 수집) - Excel(데이터 추출)
+
+### Skills
+
+- selenium
+- BeautifulSoup
+- pandas
+
+### 수집 정보
+
+- 영상 제목
+- URL
+- 조회수
+- 좋아요
+
+### 보완 필요
+
+- 댓글 수 수집
+- 일부 영상 '좋아요' html 구조 다른거 보완 필요
--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import pandas as pd
 from selenium import webdriver
 from bs4 import BeautifulSoup
-from selenium.webdriver.common.keys import Keys
-from datetime import datetime
 import time
 
 channel = "https://www.youtube.com/channel/UCHt_gfJhwWgFPE1HtG1Y-hA/videos"
@@ -13,26 +11,23 @@ driver = webdriver.Chrome()
 driver.maximize_window()
 driver.get(channel)
 
-
-lsat_height = driver.execute_script(
-    "return document.documentElement.scrollHeight")
-
 last_element = []
 video_url_list = []
 
 while True:
+    # 스크롤 내리기 => 한 번 당 영상 30개
     driver.execute_script(
         "window.scrollTo(0,document.documentElement.scrollHeight);")
-
     time.sleep(2)
+
     page = driver.page_source
     soup = BeautifulSoup(page, 'lxml')
 
     video_data = soup.find_all(
         'ytd-grid-video-renderer', {'class': 'style-scope ytd-grid-renderer'})
 
-    for i in range(5):
-        # for i in range(len(video_data)) :
+    for i in range(len(video_data)):
+
         # 동영상 제목, 개별 URL
         name = video_data[i].find('a', {'id': 'video-title'}).text
         temp_url = youtube_url + \
@@ -42,7 +37,7 @@ while True:
         url = temp_url.replace('shorts/', 'watch?v=')
 
         driver.get(url)
-        time.sleep(3)
+        time.sleep(2)
 
         driver.execute_script(
             "window.scrollTo(0,document.documentElement.scrollHeight);")
@@ -51,6 +46,8 @@ while True:
 
         each_page = driver.page_source
         each_soup = BeautifulSoup(each_page, 'lxml')
+
+        print(f'정보 수집 중 {i+1} / {len(video_data)}')
 
         # 조회수
         temp_view = each_soup.find(
@@ -62,11 +59,11 @@ while True:
             'yt-formatted-string', {'id': 'text'})
         like = temp_like[2].string
 
-        # 댓글 ing
-        temp_comments = each_soup.find_all(
-            'ytd-comments', {'id': 'comments', 'class': 'style-scope ytd-watch-flexy'})
-        ble = each_soup.find_all(
-            'ytd-item-section-renderer', {'id': 'sections'})
+        # 댓글 - 보완 필요. 수집 x
+        # temp_comments = each_soup.find_all(
+        #     'ytd-comments', {'id': 'comments', 'class': 'style-scope ytd-watch-flexy'})
+        # ble = each_soup.find_all(
+        #     'ytd-item-section-renderer', {'id': 'sections'})
 
         # 데이터 저장
         contentList.append({
@@ -75,3 +72,8 @@ while True:
             "좋아요": like,
             "URL": url
         })
+    if i + 1 == len(video_data):
+        print("수집 완료")
+        contentList = pd.DataFrame(contentList)
+        contentList.to_excel('youtube.xlsx')
+        break
